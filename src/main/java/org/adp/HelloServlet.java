725cd11fe9059ad80fb90293e903e03ee5066f11
Dheeraj <dhrajsma1998@gmail.com>
2020-07-25 13:09:49 +0530

    Project setup done

---
+++ src/main/java/org/adp/HelloServlet.java
@@ -0,0 +1,8 @@
+package org.adp;
+import javax.servlet.http.*;
+import java.io.IOException;
+public class HelloServlet extends HttpServlet {
+        public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+                httpServletResponse.getWriter().print("Hello from servlet");
+        }
+}
